3cf1610a109dcc7ed5cc6921a70fc156e8195423
Piouson <piouson@gmail.com>
2022-02-14 22:49:15 +0000

    Add more design tokens

---
+++ README.md
@@ -0,0 +1,15 @@
+# CSS Design Tokens
+
+This design tokens should be used along with your personal, internal or third-party design system to speed up frontend development.
+
+## Colors
+
+## Typography
+
+## Spacings
+
+## Hidden Content
+
+## Skip-to Links
+
+## Components
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,25 @@
 function toggleVisuallyHidden({ target }) {
-  ['start_date', 'end_date'].forEach((inputId) => {
-    document.querySelector(`label[for=${inputId}]`).classList.toggle('visually-hidden');
+  ["start_date", "end_date"].forEach((inputId) => {
+    document
+      .querySelector(`label[for=${inputId}]`)
+      .classList.toggle("visually-hidden");
   });
+}
+
+function copyTooltipText({ target }) {
+  const btn = target.nodeName === "BUTTON" ? target : target.parentElement;
+  const tooltip = btn.querySelector(".tooltip");
+  const text = btn.querySelector(".text");
+  if (tooltip.dataset.copied === "true") return;
+  navigator.clipboard.writeText(text.innerText.toLowerCase());
+  tooltip.innerText = `copied: ${text.innerText}`;
+  tooltip.dataset.copied = true;
+}
+
+function revertCopyText({ target }) {
+  const btn = target.nodeName === "BUTTON" ? target : target.parentElement;
+  const tooltip = btn.querySelector(".tooltip");
+  if (tooltip.dataset.copied !== "true") return;
+  tooltip.innerText = "copy";
+  tooltip.dataset.copied = false;
 }
